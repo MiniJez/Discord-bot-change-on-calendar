cafbc13fb9c21d8a1c350c57d84f9167d88be312
Edouard CLISSON <edouardclisson@gmail.com>
2019-02-17 17:05:18 +0100

    Feat: add v1.0 of bot

---
+++ src/bot.js
@@ -0,0 +1,141 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require('../config/config.js');
+const jsdom = require('jsdom');
+const request = require('request-promise-native');
+
+const moment = require('moment');
+moment.locale('fr');
+
+const scheduledTask = require('./scheduled-task');
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(config.token).then(() => {
+    console.log('You are connected !');
+});
+
+scheduledTask.jokeOfTheDay();
+scheduledTask.save2WeekInLocalData();
+scheduledTask.compareData();
+
+client.on('message', message => {
+    const today = moment();
+    let date;
+    let msg = '';
+
+    switch (message.content) {
+    case '!today':
+        processForOneFixedDay(today, msg, message);
+        break;
+    case '!tomorrow':
+        date = today.clone();
+        date.date(date.date() + 1);
+        processForOneFixedDay(date, msg, message);
+        break;
+    case '!week':
+        date = today.clone();
+        processForAWeekFixedDate(date, msg, message);
+        break;
+    case '!nextweek':
+        date = today.clone();
+        date.weekday(7);
+        processForAWeekFixedDate(date, msg, message);
+        break;
+    }
+});
+
+async function getData (url) {
+    return request(url);
+}
+
+function getLessonInfos (htmlBody, msg, date) {
+    const { JSDOM } = jsdom;
+    const dom = new JSDOM(htmlBody);
+    const $ = (require('jquery'))(dom.window);
+
+    let dayOfWeek = moment(date).format('dddd');
+    dayOfWeek = firstLetterToUpper(dayOfWeek);
+    let month = moment(date).format('MMMM');
+    month = firstLetterToUpper(month);
+
+    const tab = $('.Ligne');
+
+    for (let i = 0; i < tab.length; i++) {
+        var dateLesson = `${dayOfWeek} ${date.date()} ${month}`;
+        const matiere = $($(tab[i]).find('.Matiere')).html();
+        const debut = $($(tab[i]).find('.Debut')).html();
+        const fin = $($(tab[i]).find('.Fin')).html();
+        const prof = $($(tab[i]).find('.Prof')).html();
+        const salle = $($(tab[i]).find('.Salle')).html();
+
+        msg += `${dateLesson} : **${matiere}** de __${debut}__ à __${fin}__ en salle __${salle}__ avec **${prof}**\n`;
+    }
+
+    if (tab.length === 0) {
+        msg += `${dayOfWeek} ${date.date()} ${month} : Aucun cours prévu !\n`;
+    }
+
+    return msg;
+}
+
+function getUrl (day, month, year) {
+    return `http://edtmobilite.wigorservices.net/WebPsDyn.aspx?Action=posETUD&serverid=i&tel=edouard.clisson&date=${month}/${day}/${year}%208:00`;
+}
+
+function sendMessage (message, msg) {
+    message.channel.send(msg);
+}
+
+function firstLetterToUpper (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function splitMessage (msg) {
+    const halfLength = Math.floor(msg.length / 2);
+    const startMsg = msg.slice(0, halfLength);
+    const endMsg = msg.slice(halfLength, msg.length);
+
+    const splittedMsg = [];
+
+    splittedMsg.push(startMsg);
+    splittedMsg.push(endMsg);
+
+    return splittedMsg;
+}
+
+async function processForOneFixedDay (date, msg, message) {
+    const url = getUrl(date.date(), date.month() + 1, date.year());
+    const htmlBody = await getData(url);
+    msg = getLessonInfos(htmlBody, msg, date);
+
+    sendMessage(message, msg);
+}
+
+async function processForAWeekFixedDate (date, msg, message) {
+    for (let i = 0; i < 5; i++) {
+        date.weekday(i);
+        const url = getUrl(date.date(), date.month() + 1, date.year());
+        const htmlBody = await getData(url);
+        msg = getLessonInfos(htmlBody, msg, date) + '\n';
+    }
+
+    if (msg.length > 2000) {
+        const splittedMsg = splitMessage(msg);
+
+        splittedMsg.forEach(str => {
+            sendMessage(message, str);
+        });
+    } else {
+        sendMessage(message, msg);
+    }
+}
+
+module.exports.client = client;
+module.exports.getData = getData;
+module.exports.getUrl = getUrl;
+module.exports.firstLetterToUpper = firstLetterToUpper;
+module.exports.splitMessage = splitMessage;
